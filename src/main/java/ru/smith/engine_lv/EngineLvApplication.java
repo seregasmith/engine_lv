b7b72644b6be7d97c811a6e13f8dc1fe7d40ca27
smith <s.smith@bk.ru>
2022-04-16 10:46:35 +0300

    init commit Spring Boot App (WEB only), test to check context mvn wrappers and other mvn stuff

---
+++ src/main/java/ru/smith/engine_lv/EngineLvApplication.java
@@ -0,0 +1,13 @@
+package ru.smith.engine_lv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineLvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineLvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/smith/engine_lv/EngineLvApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.smith.engine_lv;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EngineLvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
